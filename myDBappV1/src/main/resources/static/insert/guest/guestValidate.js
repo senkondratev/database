8fc61c607dc2ba7c8674468bf9dd138a5a9e03be
Arseniy <a.kondratev@g.nsu.ru>
2020-05-26 19:13:06 +0700

    ю

--- myDBappV1/src/main/resources/static/insert/guest/guestValidate.js
+++
@@ -1,9 +0,0 @@
-$(document).on("change", "#reservationSelect", function () {
-    var selected = $(this).val();
-    $.get("/insert/guestValidate",
-        {
-            selectedReservationId : selected
-        }, function(html){
-            jQuery('#roomSelect').html(html);
-        });
-})
--- myDBappV1/src/main/resources/static/insert/room/roomValidate.js
+++
@@ -1,9 +0,0 @@
-$(document).on("change", "#buildingSelect", function () {
-    var selected = $(this).val();
-    $.get("/insert/roomValidate",
-        {
-            selectedBuildingId : selected
-        }, function(html){
-            jQuery('#floorSelect').html(html);
-        });
-})
