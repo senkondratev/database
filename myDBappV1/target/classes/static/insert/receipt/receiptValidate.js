9125e43221fe2660866e1ae8efeaa75092d39bed
senkondratev <sen.kondratev@gmail.com>
2020-05-27 15:46:54 +0700

    чистка

--- myDBappV1/target/classes/static/insert/receipt/receiptValidate.js
+++
@@ -1,9 +0,0 @@
-$(document).on("change", "#guestSelect", function () {
-    var selected = $(this).val();
-    $.get("/insert/receiptValidate",
-        {
-            selectedGuestId : selected
-        }, function(html){
-            jQuery('#serviceSelect').html(html);
-        });
-})
